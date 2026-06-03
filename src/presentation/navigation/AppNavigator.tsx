036529cdea88b180576efc9aa574929dbfa7425a
Rubén <ruben81003@gmail.com>
2026-06-03 19:08:18 +0200

    Añadidos permisos de cámara en app.json

--- src/presentation/navigation/AppNavigator.tsx
+++ src/presentation/navigation/AppNavigator.tsx
@@ -19,7 +19,7 @@ import ConfiguracionComponent from '../components/ConfiguracionComponent';
 import CarritoComponent from '../components/CarritoComponent';
 
 import AuthScreen from '../screens/AuthScreen';
-//import SubirProductoScreen from '../screens/SubirProductoScreen';
+import SubirProductoScreen from '../screens/SubirProductoScreen';
 import { useSelector } from 'react-redux';
 
 const Stack = createNativeStackNavigator();
@@ -178,7 +178,7 @@ function ConfiguracionStack() {
         </Stack.Navigator>
     );
 }
-/*
+
 function SubirProductoStack() {
     return (
         <Stack.Navigator id="SubirProductoStack" screenOptions={{ headerShown: false }}>
@@ -190,7 +190,7 @@ function SubirProductoStack() {
         </Stack.Navigator>
     );
 }
-*/
+
 
 export default function AppNavigator() {
     const datosUsuario = useSelector((state: any) => state.usuario);
@@ -249,6 +249,17 @@ export default function AppNavigator() {
                             ),
                         }}
                     />
+                    {estaLogueado && (
+                        <Drawer.Screen
+                            name="Subir Producto"
+                            component={SubirProductoStack}
+                            options={{
+                                drawerIcon: ({ color, size }) => (
+                                    <MaterialCommunityIcons name="cloud-upload" color={color} size={size} />
+                                ),
+                            }}
+                        />
+                    )}
                     <Drawer.Screen
                         name="Mi Carrito"
                         component={CarritoStack}
