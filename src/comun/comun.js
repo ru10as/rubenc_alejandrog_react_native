e2f9d45a16cd3fda1e315a52b7ac54fb37ce8191
alexgn12 <alexguerran2002@gmail.com>
2026-06-06 18:57:58 +0200

    Cambios visuales en el navigator y home + arreglo filtrado y alguna traduccion

--- src/comun/comun.js
+++ src/comun/comun.js
@@ -2,3 +2,7 @@ export const baseUrl = "http://172.20.10.2:3001/";
 export const colorTiendaOscuro = '#0D1B2A';
 export const colorTiendaClaro = '#E0E1DD';
 export const colorTiendaAcento = '#FB8500';
+
+// Una camiseta es de "segunda mano" si la publicó un usuario (tiene creadoPor).
+// Si no tiene creadoPor, la pusimos nosotros => es "tienda oficial".
+export const esProductoSegundaMano = (item) => !!item?.creadoPor;
